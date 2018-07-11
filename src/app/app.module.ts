b05942fb8dbdf0f8bbd913fa5c7bc6ef047c2419
tarun-palisetty <tarun.mca@gmail.com>
2018-07-11 14:41:20 +0100

    Import the library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PaymentLibModule} from 'payment-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaymentLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
