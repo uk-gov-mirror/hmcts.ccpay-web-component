4c5c80d1fccaf0564b04549de1f75ec7a041d59b
tarun-palisetty <tarun.mca@gmail.com>
2018-07-19 12:34:24 +0100

    Error service added

---
+++ projects/payment-lib/src/lib/services/shared/error-handler.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { _throw } from 'rxjs/observable/throw';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class ErrorHandlerService {
+
+  constructor() { }
+
+
+  handleError(err: HttpErrorResponse): Observable<any> {
+    let errorMessage: string;
+    if (err.error instanceof Error) {
+      // A client-side or network error occurred.
+      console.log('An error occurred: ', JSON.stringify(err));
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      console.log('Backend status error: ', err.status);
+      errorMessage = `${err.error}`;
+    }
+    return _throw(errorMessage);
+  }
+}
