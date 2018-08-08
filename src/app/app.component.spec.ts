8d66447118081dd3ab3dca2617f31f33ab27719a
tarun-palisetty <tarun.mca@gmail.com>
2018-08-08 15:50:40 +0100

    Payments by ccdCaseNumber end-point updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { PhaseBannerComponent } from './components/shared/phase-banner/phase-banner.component';
+import { CaseListComponent } from './components/case-list/case-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        NavigationComponent,
+        PhaseBannerComponent,
+        CaseListComponent
       ],
+      imports: [RouterTestingModule.withRoutes([
+        { path: 'cases', component: CaseListComponent }
+      ])]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pay-web-app!');
-  }));
+
 });
