55f5ddbaeef8f96b11b8e132125d0d58ee59ddbe
tarun-palisetty <tarun.mca@gmail.com>
2019-06-25 13:27:06 +0100

    Payment details table added

--- projects/payment-lib/src/lib/components/case-transactions/case-transactions.component.ts
+++ projects/payment-lib/src/lib/components/case-transactions/case-transactions.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PaymentLibComponent} from '../../payment-lib.component';
 import {HttpClient} from '@angular/common/http';
+import {PaymentListService} from '../../services/payment-list/payment-list.service';
+import {IPayments} from '../../interfaces/IPayments';
 
 @Component({
   selector: 'ccpay-case-transactions',
@@ -9,11 +11,20 @@ import {HttpClient} from '@angular/common/http';
 })
 export class CaseTransactionsComponent implements OnInit {
   ccdCaseNumber: string;
+  payments: IPayments;
+  errorMessage: string;
+  code: string;
 
-  constructor(private paymentLibComponent: PaymentLibComponent,
-              private http: HttpClient) { }
+  constructor(private paymentListService: PaymentListService,
+              private paymentLibComponent: PaymentLibComponent) { }
 
   ngOnInit() {
     this.ccdCaseNumber = this.paymentLibComponent.CCD_CASE_NUMBER;
+
+    this.paymentListService.getPaymentByCcdCaseNumber(this.paymentLibComponent.CCD_CASE_NUMBER, this.paymentLibComponent.PAYMENT_METHOD)
+      .subscribe(
+        payments => this.payments = payments,
+        (error: any) => this.errorMessage = <any>error
+      );
   }
 }
