7616f790f31cf9e7df43458771922f9ac22bfb05
kumarmadurai <kumarmcamadurai@gmail.com>
2019-07-18 14:44:46 +0100

    WIP

--- projects/payment-lib/src/lib/components/case-transactions/case-transactions.component.spec.ts
+++ projects/payment-lib/src/lib/components/case-transactions/case-transactions.component.spec.ts
@@ -1,13 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CommonModule } from '@angular/common';
-import { PaymentViewService } from '../../services/payment-view/payment-view.service';
-import { PaymentLibComponent } from '../../payment-lib.component';
-import { HttpClientModule } from '@angular/common/http';
 
 import { CaseTransactionsComponent } from './case-transactions.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CaseTransactionsComponent', () => {
   let component: CaseTransactionsComponent;
@@ -29,42 +22,4 @@ describe('CaseTransactionsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
-
-
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { CommonModule } from '@angular/common';
-import { FeeSummaryComponent } from './fee-summary.component';
-import { HttpClientModule } from '@angular/common/http';
-import { PaymentViewService } from '../../services/payment-view/payment-view.service';
-import { PaymentLibComponent } from '../../payment-lib.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-
-describe('Fee Summary component', () => {
-  let component: FeeSummaryComponent,
-  fixture: ComponentFixture<FeeSummaryComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FeeSummaryComponent],
-      providers: [PaymentViewService, PaymentLibComponent,
-        { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } }],
-      imports: [
-        CommonModule,
-        HttpClientModule,
-        RouterModule
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    });
-
-    fixture = TestBed.createComponent(FeeSummaryComponent);
-    component = fixture.componentInstance;
-  });
-
-  it('Should create', () => {
-    expect(component).toBeTruthy();
-  });
-
 });
