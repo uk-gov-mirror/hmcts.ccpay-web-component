62055b00964f65e81adc6a75522eaf5687a13ae0
Elviani Huseyin <elviani_s@hotmail.com>
2019-07-25 14:27:53 +0100

    remove the hardcoded payment group ref

--- projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
+++ projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
@@ -33,7 +33,7 @@ export class FeeSummaryComponent implements OnInit {
     this.viewStatus = 'main';
 
   //  this.paymentGroupRef = '2018-15310089885';
-   this.paymentGroupRef = '2019-15496299273';
+  //  this.paymentGroupRef = '2019-15496299273';
 
     this.paymentViewService.getPaymentGroupDetails(this.paymentGroupRef,
       this.paymentLibComponent.paymentMethod).subscribe(
