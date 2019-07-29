eb7d311749b48c34bd4045f8696fa7e019541322
kumarmadurai <kumarmcamadurai@gmail.com>
2019-07-29 12:59:57 +0100

    PR Comments fixes

--- projects/payment-lib/src/lib/pipes/add-hyphens.spec.ts
+++ projects/payment-lib/src/lib/pipes/add-hyphens.spec.ts
@@ -12,14 +12,15 @@ describe('Add hyphens pipe', () => {
       });
   });
 
-  it('Should create an instance and tranform',() => {
+  it('Should create an instance for add hyphen pipe and transform non-hyphenated CCD number to hyphenated CCD number',() => {
+  
     const pipe = new AddHyphensPipe();
     expect(pipe).toBeTruthy();
     expect(pipe.transform('1111222233334444')).toBe('1111-2222-3333-4444');
 
   });
 
-  it('Should transform', () => {
+  it('Should not transform hyphenated CCD number', () => {
     const pipe = new AddHyphensPipe();
     expect(pipe.transform('1111-2222-3333-4444')).toBe('1111-2222-3333-4444');
   });
