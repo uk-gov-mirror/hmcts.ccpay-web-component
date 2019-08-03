cbea4de273b4d4a5c29221c1422f358b02233b80
kumarmadurai <kumarmcamadurai@gmail.com>
2019-08-03 11:29:19 +0100

    removing static value assignment

--- projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
+++ projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
@@ -36,7 +36,7 @@ export class FeeSummaryComponent implements OnInit {
   ngOnInit() {
     this.viewStatus = 'main';
     // this.paymentGroupRef = '2018-15310089885';
-    this.paymentGroupRef = '2019-15496299273';
+    //this.paymentGroupRef = '2019-15496299273';
     this.getPaymentGroup();
   }
 
