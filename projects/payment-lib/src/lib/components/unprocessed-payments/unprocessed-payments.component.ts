08c962c602eff815fcea2f33ffbb051ba9553b03
kumarmadurai <kumarmcamadurai@gmail.com>
2019-08-16 17:05:56 +0100

    PAY-2043 card changes

--- projects/payment-lib/src/lib/components/unprocessed-payments/unprocessed-payments.component.ts
+++ projects/payment-lib/src/lib/components/unprocessed-payments/unprocessed-payments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BulkScaningPaymentService } from '../../services/bulk-scaning-payment/bulk-scaning-payment.service';
 import { PaymentLibComponent } from '../../payment-lib.component';
+import { IBSPayments } from '../../interfaces/IBSPayments';
 
 @Component({
   selector: 'app-unprocessed-payments',
@@ -9,10 +10,35 @@ import { PaymentLibComponent } from '../../payment-lib.component';
 })
 export class UnprocessedPaymentsComponent implements OnInit {
   viewStatus = 'main';
+  unassignedRecordList: IBSPayments;
+  errorMessage: string;
+  ccdCaseNumber:string;
+  recordId: string = null;
+
   constructor(private bulkScaningPaymentService: BulkScaningPaymentService,
     private paymentLibComponent: PaymentLibComponent) { }
 
   ngOnInit() {
     //Todo ...
+    this.ccdCaseNumber = this.paymentLibComponent.CCD_CASE_NUMBER;
+    this.getUnassignedPaymentlist();
+
+  }
+
+  getUnassignedPaymentlist() {
+    this.bulkScaningPaymentService.getBSPayments(`CCD-${this.ccdCaseNumber}`).subscribe(
+      unassignedPayments => {
+       this.unassignedRecordList = unassignedPayments['unassigned_payments'];
+      },
+      (error: any) => this.errorMessage = error
+    );
+  }
+  formatUnassignedRecordId(ID: Number){
+    return `unassignrecord-${ID}`;
+  }
+  loadUnsolicitedPage(viewName: string) {
+    alert(this.recordId);
+    this.paymentLibComponent.bspaymentdcn = `DCN-${this.ccdCaseNumber}`;
+    this.paymentLibComponent.viewName = viewName;
   }
 }
