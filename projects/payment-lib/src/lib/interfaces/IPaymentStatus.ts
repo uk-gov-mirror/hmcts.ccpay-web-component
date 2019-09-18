92188df647cd7ffc3923c4a959fc386fbc7ede49
kumarmadurai <kumarmcamadurai@gmail.com>
2019-09-18 20:40:31 +0100

    Updating packge json version

---
+++ projects/payment-lib/src/lib/interfaces/IPaymentStatus.ts
@@ -0,0 +1,11 @@
+export class IPaymentStatus {
+  payment_allocation_status: any;
+  date_created: string;
+  payment_group_reference: string;
+  payment_reference: string;
+  receiving_office: string;
+  receiving_email_address: string;
+  sending_email_address: string;
+  unidentified_reason: string;
+  user_id: string;
+}
