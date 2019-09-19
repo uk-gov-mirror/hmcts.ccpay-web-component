a2b785c3210626e767e6bdae58c6ee3d3a9d6087
kumarmadurai <kumarmcamadurai@gmail.com>
2019-09-19 21:41:37 +0100

    disable status history section for unsolicited and unidentified

--- projects/payment-lib/src/lib/components/payment-view/payment-view.component.ts
+++ projects/payment-lib/src/lib/components/payment-view/payment-view.component.ts
@@ -33,9 +33,8 @@ export class PaymentViewComponent implements OnInit {
         this.paymentGroup = paymentGroup;
         this.paymentGroup.payments = this.paymentGroup.payments.filter
         (paymentGroupObj => paymentGroupObj['reference'].includes(this.paymentLibComponent.paymentReference));
-        const paymentAllocation = this.paymentGroup.payments[0].payment_allocation,
-        paymentAllocationStatus = paymentAllocation.length > 0 ? paymentAllocation[0].payment_allocation_status : null;
-        this.isStatusAllocated = paymentAllocationStatus && paymentAllocationStatus.name === 'Allocated' || paymentAllocationStatus;
+        const paymentAllocation = this.paymentGroup.payments[0].payment_allocation;
+        this.isStatusAllocated = paymentAllocation.length > 0 && paymentAllocation[0].payment_allocation_status.name === 'Allocated' || paymentAllocation.length === 0;
       },  
       (error: any) => this.errorMessage = error
     );
