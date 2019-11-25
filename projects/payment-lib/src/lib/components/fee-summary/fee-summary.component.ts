3224b339fa29360a20126cc5668b31ab9539b362
“Kumar <Kumarmcamadura@gmail.com>
2019-11-25 16:19:49 +0000

    fixing back button changes

--- projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
+++ projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
@@ -52,6 +52,15 @@ export class FeeSummaryComponent implements OnInit {
     this.selectedOption = this.paymentLibComponent.SELECTED_OPTION.toLocaleLowerCase();
     //this.paymentGroupRef = '2018-15310089885';
     //this.paymentGroupRef = '2019-15496299273';
+    this.paymentViewService.getBSfeature().subscribe(
+      features => {
+        let result = JSON.parse(features).filter(feature => feature.uid === BS_ENABLE_FLAG);
+        this.paymentLibComponent.ISBSENABLE = result[0] ? result[0].enable : false;
+      },
+      err => {
+        this.paymentLibComponent.ISBSENABLE = false;
+      }
+    );
     if (this.bsPaymentDcnNumber) {
       this.getUnassignedPaymentlist();
     }
