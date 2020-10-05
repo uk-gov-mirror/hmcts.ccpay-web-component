0b8388fa08dcd270e5bec7605212f7af5d3e8060
“Kumar <Kumarmcamadura@gmail.com>
2020-10-05 12:12:48 +0100

    PAY-3688 DATA loss new line issue

--- projects/payment-lib/src/lib/components/reports/reports.component.ts
+++ projects/payment-lib/src/lib/components/reports/reports.component.ts
@@ -96,6 +96,15 @@ downloadReport(){
                 res['data'][i]['env_ref'] = res['data'][i]["payment_asset_dcn"].substr(0,13);
                 res['data'][i]['env_item'] = res['data'][i]["payment_asset_dcn"].substr(13,21);
               }
+              if(res['data'][i]["amount"] !== undefined) {
+                res['data'][i]['amount'] = this.convertToFloatValue(res['data'][i]['amount']);
+              }
+              if(res['data'][i]["balance"] !== undefined) {
+                res['data'][i]['balance'] = this.convertToFloatValue(res['data'][i]["balance"]);
+              }
+              if(res['data'][i]["payment_amount"] !== undefined) {
+                res['data'][i]['payment_amount'] = this.convertToFloatValue(res['data'][i]['payment_amount']);
+              }
             }
           } 
           this.isDownLoadButtondisabled = false;
@@ -117,6 +126,9 @@ downloadReport(){
           }
           if(res['data'].length > 1) {
           for( var i=0; i< res['data'].length; i++) {
+            if(res['data'][i]["amount"] !== undefined) {
+              res['data'][i]['amount'] = this.convertToFloatValue(res['data'][i]['amount']);
+            }
             if(res['data'][i]["payment_asset_dcn"] !== undefined) {
             res['data'][i]['env_ref'] = res['data'][i]["payment_asset_dcn"].substr(0,13);
             res['data'][i]['env_item'] = res['data'][i]["payment_asset_dcn"].substr(13,21);
