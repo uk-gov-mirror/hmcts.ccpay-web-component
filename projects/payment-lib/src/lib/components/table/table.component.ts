f8a6aeb762442189a3bc5e4f7dc363bcde78f3dd
santu412 <santu412@gmail.com>
2021-08-26 17:05:12 +0100

    refunds

--- projects/payment-lib/src/lib/components/table/table.component.ts
+++ projects/payment-lib/src/lib/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, Input} from '@angular/core';
+import {Component, ViewChild, Input, ChangeDetectorRef} from '@angular/core';
 import {PaymentLibComponent} from '../../payment-lib.component';
 import { MatTableDataSource} from '@angular/material/table';
 import {MatSort } from '@angular/material/sort';
@@ -24,123 +24,13 @@ export class TableComponent {
   count: number;
   refundList: IRefundList[];
 
-  usersData = [
-    {
-      "refundReference": "RF-1629-8342-2480-9302",
-      "amount": "100",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC1",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER1 USER1",
-      "updateBy": "USER1",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    },
-    {
-      "refundReference": "RF-1629-8342-6657-5019",
-      "amount": "200",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC2",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER2 USER2",
-      "updateBy": "USER2",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    }, 
-    {
-      "refundReference": "RF-1629-8342-9397-9705",
-      "amount": "300",
-      "reason": "reason3 hhhh hhh hhh hhh hhhh",
-      "status": "SUBMITTED",
-      "payment_reference": "RC3",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER3 USER3",
-      "updateBy": "USER3",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    }, 
-    {
-      "refundReference": "RF-1629-8343-2001-6182",
-      "amount": "400",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC4",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER4 USER4",
-      "updateBy": "USER4",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    },
- 
-    {
-      "refundReference": "RF-1629-8343-6158-7661",
-      "amount": "500",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC5",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER5 USER5",
-      "updateBy": "USER5",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    },
-    {
-      "refund_reference": "RF-1324-6846-2454-5450",
-      "amount": "600",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC6",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER6 USER6",
-      "updateBy": "USER6",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    },
-    {
-      "refund_reference": "RF-1324-6846-2454-5451",
-      "amount": "700",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC7",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER7 USER7",
-      "updateBy": "USER7",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    },
-    {
-      "refund_reference": "RF-1324-6846-2454-5452",
-      "amount": "800",
-      "reason": "Case Withdrawn",
-      "status": "SUBMITTED",
-      "payment_reference": "RC8",
-      "ccd_case_number": "1010-1010-1010-1010",
-      "user_full_name": "USER8 USER8",
-      "updateBy": "USER8",
-      "date_created": "2021-07-20T09:12:52.778Z",
-      "date_updated": "2021-07-20T09:12:52.778Z"
-    }
-  ];
-
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
-    private paymentLibComponent: PaymentLibComponent
-  ) {
-
-    // Assign the data to the data source for the table to render
-   //  this.dataSource = new MatTableDataSource(this.usersData);
-    // this.dataSource = new MatTableDataSource(this.refundList);
-    // this.actualcount = this.dataSource.data.length;
-    // if( this.refundList !== undefined) {
-    // this.userLst = this.refundList.reduce((r,{user_full_name}) => (r[user_full_name]='', r) , {});
-    //  this.userLst = Object.keys(this.userLst);
-    // }
-  }
+    private paymentLibComponent: PaymentLibComponent,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() { 
     this.errorMessage = this.errorMessage;
@@ -164,7 +54,7 @@ export class TableComponent {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-
+    this.cdRef.detectChanges();
   }
 
   applyFilter(filterValue: string) {
@@ -182,12 +72,4 @@ export class TableComponent {
     this.paymentLibComponent.refundReference = refundReference;
     this.paymentLibComponent.viewName = 'process-refund';
   }
-}
-
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
 }
