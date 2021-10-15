46c4560a28a5a4fae08e51d6c7d403dc9551e96b
santu412 <santu412@gmail.com>
2021-10-15 10:16:11 +0100

    refunds

--- projects/payment-lib/src/lib/services/refunds/refunds.service.ts
+++ projects/payment-lib/src/lib/services/refunds/refunds.service.ts
@@ -52,8 +52,8 @@ export class RefundsService {
   }
 
 patchRefundActions(body:IPatchRefundAction, refundReference: string, reviewerAction: string): Observable<any> {
-  const opts = this.addHeaders({});
-  return this.http.patch<any>(`${this.paymentLibService.REFUNDS_API_ROOT}/${refundReference}/action/${reviewerAction}`, body, opts)
+  // const opts = this.addHeaders({});
+  return this.http.patch<any>(`${this.paymentLibService.REFUNDS_API_ROOT}/${refundReference}/action/${reviewerAction}`, body)
   .pipe(
     catchError(this.errorHandlerService.handleError)
   );
@@ -104,8 +104,8 @@ postIssueRefund(body: IssueRefundRequest): Observable<any> {
 }
 
 patchResubmitRefund(body: IResubmitRefundRequest, refund_reference: string): Observable<any> {
-  const opts = this.addHeaders({});
-  return this.http.patch<any>(`${this.paymentLibService.REFUNDS_API_ROOT}/resubmit/${refund_reference}`, body,opts).pipe(
+ // const opts = this.addHeaders({});
+  return this.http.patch<any>(`${this.paymentLibService.REFUNDS_API_ROOT}/resubmit/${refund_reference}`, body).pipe(
     catchError(this.errorHandlerService.handleError)
   );
 }
