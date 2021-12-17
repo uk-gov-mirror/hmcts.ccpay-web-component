d51e4cb3260a5cd122105afd452400acb3247728
santu412 <santu412@gmail.com>
2021-12-17 10:44:35 +0000

    refunds

--- projects/payment-lib/src/lib/components/refund-status/refund-status.component.ts
+++ projects/payment-lib/src/lib/components/refund-status/refund-status.component.ts
@@ -53,7 +53,7 @@ export class RefundStatusComponent implements OnInit {
   isLastUpdatedByCurrentUser: boolean = true;
   isProcessRefund: boolean = false;
   changedAmount: number;
-  // allowedRolesToAccessRefund = ['payments-refund-approver', 'payments-refund'];
+  allowedRolesToAccessRefund = ['payments-refund-approver', 'payments-refund'];
 
   constructor(private formBuilder: FormBuilder,
     private refundService: RefundsService,
@@ -64,7 +64,7 @@ export class RefundStatusComponent implements OnInit {
 
   ngOnInit() {
 
-   // if (this.check4AllowedRoles2AccessRefund()) {
+   if (this.check4AllowedRoles2AccessRefund()) {
     this.resetRemissionForm([false, false, false, false], 'All');
     this.bsPaymentDcnNumber = this.paymentLibComponent.bspaymentdcn;
     this.isCallFromRefundList = this.paymentLibComponent.isCallFromRefundList;
@@ -111,15 +111,15 @@ export class RefundStatusComponent implements OnInit {
           this.refundButtonState = this.refundlist.refund_status.name;
         }
       }
-   // }
+   }
   }
   
 
-  // check4AllowedRoles2AccessRefund = (): boolean => {
-  //   return this.allowedRolesToAccessRefund.some(role =>
-  //     this.LOGGEDINUSERROLES.indexOf(role) !== -1
-  //   );
-  // }
+  check4AllowedRoles2AccessRefund = (): boolean => {
+    return this.allowedRolesToAccessRefund.some(role =>
+      this.LOGGEDINUSERROLES.indexOf(role) !== -1
+    );
+  }
 
   getRefundsStatusHistoryList() {
     if(this.refundlist !== undefined) {
