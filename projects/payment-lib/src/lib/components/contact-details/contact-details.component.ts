b5b169efc302f0e2cead251a8cdfb53ef12104cb
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-01-12 23:06:58 +0000

    Capture address from user

--- projects/payment-lib/src/lib/components/contact-details/contact-details.component.ts
+++ projects/payment-lib/src/lib/components/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { PaymentLibComponent } from '../../payment-lib.component';
 
 @Component({
@@ -12,16 +13,71 @@ export class ContactDetailsComponent implements OnInit {
   isEmailSAddressClicked: boolean = true;
   isPostcodeClicked: boolean = false;
   isManualAddressClicked: boolean = false;
+  emailAddressForm: FormGroup;
+  postCodeForm: FormGroup;
+  manualAddressForm: FormGroup;
 
+  isEmailEmpty: boolean = false;
+  emailHasError: boolean = false;
+  isPostcodeEmpty: boolean = false;
+  postcodeHasError: boolean = false;
+  isaddressLine1Empty: boolean = false;
+  addressLine1HasError: boolean = false;
+  addressLine2HasError: boolean = false;
+  isTownOrCityEmpty: boolean = false;
+  townOrCityHasError: boolean = false;
+  isCountyEmpty: boolean = false;
+  countyHasError: boolean = false;
+  isMPostcodeEmpty: boolean = false;
+  mpostcodeHasError: boolean = false;
+  isCountryEmpty: boolean = false;
 
-  constructor(
+  constructor(private formBuilder: FormBuilder,
               private paymentLibComponent: PaymentLibComponent) { }
 
   ngOnInit() {
-    
+    this.resetForm([false,false,false,false,false,false,false,false,false,false,false,false,false,false], 'all');
+
+    this.emailAddressForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$')
+      ]))
+    });
+    this.postCodeForm = this.formBuilder.group({
+      postcode: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^([A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?[0-9][A-Z]{2})')
+      ]))
+    });
+    this.manualAddressForm = this.formBuilder.group({
+      addressl1: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9\s,\'-]*$')
+      ])),
+      addressl2: new FormControl('', Validators.compose([
+        Validators.pattern('^[a-zA-Z0-9\s,\'-]*$')
+      ])),
+      townorcity: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9\s,\'-]*$')
+      ])),
+      county: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9\s,\'-]*$')
+      ])),
+      mpostcode: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^([A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?[0-9][A-Z]{2})')
+      ])),
+      country: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
+    });
   }
 
   selectContactOption(type, isLinkedClied) {
+    this.resetForm([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false], 'all');
     if( type === 'Email' && isLinkedClied === 'false'){
       this.isEmailSAddressClicked = true;
       this.isPostcodeClicked = false;
@@ -39,14 +95,105 @@ export class ContactDetailsComponent implements OnInit {
   }
 
   finalFormSubmit() {
+    this.resetForm([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false], 'all');
     if( this.isEmailSAddressClicked ){
+      const emailField = this.emailAddressForm.controls.email;
+      if (this.emailAddressForm.dirty && this.emailAddressForm.valid) {
 
+      } else {
+        if( emailField.value == '' ) {
+          this.resetForm([true,false,false,false,false,false,false,false,false,false,false,false,false,false], 'email');
+        }
+        if(emailField.value != '' && emailField.invalid ) {
+          this.resetForm([false,true,false,false,false,false,false,false,false,false,false,false,false,false], 'email');
+        }
+      }
     } else if( this.isPostcodeClicked && !this.isManualAddressClicked ) {
-
+      this.postcodeValidation();
     } else if(this.isPostcodeClicked && this.isManualAddressClicked) {
+      const fieldCtrls = this.manualAddressForm.controls;
+      if (this.manualAddressForm.dirty && this.manualAddressForm.valid) {
+
+      } else {
+        if( fieldCtrls.addressl1.value == '' ) {
+          this.resetForm([false,false,false,false,true,false,false,false,false,false,false,false,false,false], 'address1');
+        }
+        if(fieldCtrls.addressl1.value != '' && fieldCtrls.addressl1.invalid ) {
+          this.resetForm([false,false,false,false,false,true,false,false,false,false,false,false,false,false], 'address1');
+        }
+        if(fieldCtrls.addressl2.value != '' && fieldCtrls.addressl2.invalid ) {
+          this.resetForm([false,false,false,false,false,false,true,false,false,false,false,false,false,false], 'address2');
+        }
+        if( fieldCtrls.townorcity.value == '' ) {
+          this.resetForm([false,false,false,false,false,false,false,true,false,false,false,false,false,false], 'town');
+        }
+        if(fieldCtrls.townorcity.value != '' && fieldCtrls.townorcity.invalid ) {
+          this.resetForm([false,false,false,false,false,false,false,false,true,false,false,false,false,false], 'town');
+        }
+        if( fieldCtrls.county.value == '' ) {
+          this.resetForm([false,false,false,false,false,false,false,false,false,true,false,false,false,false], 'county');
+        }
+        if(fieldCtrls.county.value != '' && fieldCtrls.county.invalid ) {
+          this.resetForm([false,false,false,false,false,false,false,false,false,false,true,false,false,false], 'county');
+        }
+        if( fieldCtrls.mpostcode.value == '' ) {
+          this.resetForm([false,false,false,false,false,false,false,false,false,false,false,true,false,false], 'mpostcode');
+        }
+        if(fieldCtrls.mpostcode.value != '' && fieldCtrls.mpostcode.invalid ) {
+          this.resetForm([false,false,false,false,false,false,false,false,false,false,false,false,true,false], 'mpostcode');
+        }
+        if( fieldCtrls.country.value == '' ) {
+          this.resetForm([false,false,false,false,false,false,false,false,false,false,false,false,false,true], 'country');
+        }
+      }
 
     }
 
   }
+  postcodeValidation() {
+    const postcodeField = this.postCodeForm.controls.postcode;
+    if (this.postCodeForm.dirty && this.postCodeForm.valid) {
 
+    } else {
+      if( postcodeField.value == '' ) {
+        this.resetForm([false,false,true,false,false,false,false,false,false,false,false,false,false], 'postcode');
+      }
+      if(postcodeField.value != '' && postcodeField.invalid ) {
+        this.resetForm([false,false,false,true,false,false,false,false,false,false,false,false,false], 'postcode');
+      }
+    }
+  }
+  resetForm(val, field) {
+    if(field==='email' || field==='all') {
+      this.isEmailEmpty = val[0];
+      this.emailHasError = val[1];
+    }
+    if(field==='postcode' || field==='all') {
+      this.isPostcodeEmpty = val[2];
+      this.postcodeHasError = val[3];
+    }
+    if(field==='address1' || field==='all') {
+      this.isaddressLine1Empty = val[4];
+      this.addressLine1HasError = val[5];
+    }
+    if(field==='address2' || field==='all') {
+      this.addressLine2HasError = val[6];
+    }
+    if(field==='town' || field==='all') {
+      this.isTownOrCityEmpty = val[7];
+      this.townOrCityHasError = val[8];
+    }
+    if(field==='county' || field==='all') {
+      this.isCountyEmpty = val[9];
+      this.countyHasError = val[10];
+    }
+    if(field==='mpostcode' || field==='all') {
+      this.isMPostcodeEmpty = val[11];
+      this.mpostcodeHasError = val[12];
+    }
+    if(field==='country' || field==='all') {
+      this.isCountryEmpty = val[13];
+    }
+  
+  }
 }
