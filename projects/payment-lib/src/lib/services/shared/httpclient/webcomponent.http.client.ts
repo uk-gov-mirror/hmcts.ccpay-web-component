b12d6a922dc53d3928403258cf015c2dcb0b665c
santu412 <santu412@gmail.com>
2022-02-08 17:08:37 +0000

    Partial refunds

--- projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.ts
+++ projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.ts
@@ -46,7 +46,13 @@ export class WebComponentHttpClient {
     }
     headers['X-Requested-With'] = 'XMLHttpRequest';
     if (csrfToken === null) {
-      headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith(' XSRF-TOKEN')).split('=')[1];
+      if( document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')).split('=')[1] !== undefined) {
+        headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')).split('=')[1];
+        
+      } else {
+        headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith(' XSRF-TOKEN')).split('=')[1];
+      }
+      
     } else {
       headers['CSRF-Token'] = csrfToken.content;
     }
