7eb65f4b35035a1157953e72aca80a7b418ae7a2
santu412 <santu412@gmail.com>
2022-03-01 13:03:58 +0000

    browserfix

--- projects/payment-lib/src/lib/services/refunds/refunds.service.ts
+++ projects/payment-lib/src/lib/services/refunds/refunds.service.ts
@@ -120,7 +120,7 @@ addHeaders(options: any): any {
   }
   headers['X-Requested-With'] = 'XMLHttpRequest';
   if (csrfToken.content === null) {
-    if( document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN') !== undefined )) {
+    if( document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')) !== undefined ) {
       headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')).split('=')[1];     
     } else {
       headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith(' XSRF-TOKEN')).split('=')[1];
--- projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.ts
+++ projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.ts
@@ -46,7 +46,7 @@ export class WebComponentHttpClient {
     }
     headers['X-Requested-With'] = 'XMLHttpRequest';
     if (csrfToken === null) {
-      if( document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN') !== undefined )) {
+      if( document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')) !== undefined ) {
         headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith('XSRF-TOKEN')).split('=')[1];
       } else {
         headers['CSRF-Token'] = document.cookie.split(';').find(row => row.startsWith(' XSRF-TOKEN')).split('=')[1];
