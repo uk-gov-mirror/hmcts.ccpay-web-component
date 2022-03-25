d566b2147ab6c31d6f1da8609b47a42d7cf1c8e5
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-03-25 10:50:49 +0000

    final code commit for W2P

--- projects/payment-lib/src/lib/components/add-remission/add-remission.component.ts
+++ projects/payment-lib/src/lib/components/add-remission/add-remission.component.ts
@@ -811,6 +811,7 @@ export class AddRemissionComponent implements OnInit {
     }
     if(isFullyRefund) {
       this.totalRefundAmount = this.payment.amount;
+      this.fees = <any>this.paymentGroup.fees[0];
     }
     this.fees = this.remissionForm.value.feesList.filter(value => value.selected===true);
   
