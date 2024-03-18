b5fd6a680c9e72d82aa4f364c2a0a29ff27b1fc9
Victoria Hulse <Victoria.Hulse@HMCTS.NET>
2024-03-18 20:40:28 +0000

    array sort data loss

--- projects/payment-lib/src/lib/services/xl-file/xl-file.service.ts
+++ projects/payment-lib/src/lib/services/xl-file/xl-file.service.ts
@@ -16,11 +16,18 @@ export class XlFileService {
 
     if(excelFileName.match('Data_Loss')!== null){
       //worksheet =  XLSX.utils.json_to_sheet(json,{header:['loss_resp','payment_asset_dcn','env_ref','env_item','resp_service_id','resp_service_name','date_banked','bgc_batch','payment_method','amount']});
+  // console.log("excelFileName: " +  toString(), excelFileName);
+    // console.log("test2: " );
+
       worksheet =  this.setDataLossReportHeaders(worksheet);
       worksheet = this.autoFitColumns(worksheet,json);
       worksheet =  this.addRowData(worksheet,json);
+  // console.log("excelFileName: " +  toString(), json);
+
     } else if(excelFileName.match('Unprocessed')!== null){
       //worksheet =  XLSX.utils.json_to_sheet(json,{header:['resp_service_id','resp_service_name','exception_ref','ccd_ref','date_banked','bgc_batch','payment_asset_dcn','env_ref','env_item','payment_method','amount']});
+ //console.log("excelFileName: " +  toString(), excelFileName);
+ // console.log("test3: " );
       worksheet =  this.setUnprocessedReportHeaders(worksheet);
       worksheet = this.autoFitColumns(worksheet,json);
       worksheet =  this.addRowData(worksheet,json);
@@ -36,6 +43,9 @@ export class XlFileService {
       worksheet =  this.addRowData(worksheet,json);
     } else {
       //worksheet =  XLSX.utils.json_to_sheet(json,{header:['resp_service_id','resp_service_name','surplus_shortfall','balance','payment_amount','ccd_case_reference', 'ccd_exception_reference', 'processed_date', 'reason', 'explanation', 'user_name']});
+    // console.log("excelFileName: " +  toString(), excelFileName);
+ //console.debug("excelFileName: " +  toString(), excelFileName);
+ // console.log("test5: " );
       worksheet =  this.setShortFallReportHeaders(worksheet);
       worksheet = this.autoFitColumns(worksheet,json);
       worksheet =  this.addRowData(worksheet,json);
@@ -47,30 +57,40 @@ export class XlFileService {
           FileSaver.saveAs(blob, `${excelFileName + EXCEL_EXTENSION}`);
     });
 
-    //const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    //this.saveAsExcelFile(excelBuffer, excelFileName);
+
   }
 
 
 private autoFitColumns (worksheet: ExcelJS.Worksheet,json:any) : ExcelJS.Worksheet {
   let objectMaxLength = [];
   let ColWidth = [];
-  let obj = <any>Object;
+  let obj = (Object as any);
+
     for (let i = 0; i < json.length; i++) {
+
       let value = obj.values(json[i]);
       let key = obj.keys(json[i]);
+
+
+
       for (let j = 0; j < value.length; j++) {
+
+
         if(value[j] === null){
           value[j] = '';
+
         }
         objectMaxLength[j] =
           key[j].length >= value[j].length
+
             ? key[j].length+2
             : value[j].length+1;
+
             if( value[j].length === undefined){
               objectMaxLength[j] =  key[j].length+2;
+
             }
-      ColWidth.push({'width': +objectMaxLength[j]});
+      ColWidth.push({'width': + objectMaxLength[j]});
       }
     }
     worksheet.columns = ColWidth;
@@ -83,7 +103,39 @@ private addRowData (worksheet: ExcelJS.Worksheet,json:any) : ExcelJS.Worksheet {
   let obj = <any>Object;
     for (let i = 0; i < json.length; i++) {
       let rowData = obj.values(json[i]);
-      worksheet.addRow(rowData);
+       let values = [];
+            let key = obj.keys(json[i]);
+
+
+
+
+
+ for (let j = 0; j < (key.length+3); j++) {
+
+
+
+
+       if(rowData[j] === null){
+
+            rowData[j] = '';
+
+       }
+        values[0] = (rowData[3]);
+          values[1] = (rowData[4]);
+             values[2] = (rowData[8]);
+                values[3] = (rowData[9]);
+                   values[4] = (rowData[0]);
+                      values[5] = (rowData[5]);
+                         values[6] = (rowData[2]);
+                            values[7] = (rowData[6]);
+                              values[8] = (rowData[1]);
+                                values[9] = (rowData[7]);
+
+
+
+      }
+     worksheet.addRow(values);
+
     }
     return worksheet;
 }
