e53245970df03af053ced51649131384ed8d0bee
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-05-21 16:31:11 +0100

    PAY-7130 use empty stubs

--- projects/payment-lib/src/lib/components/service-request/service-request.component.spec.ts
+++ projects/payment-lib/src/lib/components/service-request/service-request.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Pipe, PipeTransform } from '@angular/core';
 import { ServiceRequestComponent } from './service-request.component';
+import {PaymentViewService} from "../../services/payment-view/payment-view.service";
+import {NotificationService} from "../../services/notification/notification.service";
 
 @Pipe({ name: 'rpxTranslate' })
 class RpxTranslateMockPipe implements PipeTransform {
@@ -13,12 +15,15 @@ describe('ServiceRequestComponent', () => {
   let component: ServiceRequestComponent;
   let fixture: ComponentFixture<ServiceRequestComponent>;
   const paymentLibComponentStub = () => ({ viewName: {} });
+  const emptyServiceStub = () => ({  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RpxTranslateMockPipe ],
       providers: [
         { provide: 'PAYMENT_LIB', useFactory: paymentLibComponentStub },
+        { provide: PaymentViewService, useFactory: emptyServiceStub },
+        { provide: NotificationService, useFactory: emptyServiceStub }
       ]
     })
     .compileComponents();
