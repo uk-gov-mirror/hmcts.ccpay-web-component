f7eca825e4b7ffbc4a7dca35fdef97ab09940125
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-05-24 11:47:55 +0100

    PAY-7130 stub payment view service

--- projects/payment-lib/src/lib/components/pba-details/pba-details.component.spec.ts
+++ projects/payment-lib/src/lib/components/pba-details/pba-details.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { PbaDetailsComponent } from './pba-details.component';
+import { PaymentViewService } from "../../services/payment-view/payment-view.service"
 
 describe('PbaDetailsComponent', () => {
   let component: PbaDetailsComponent;
   let fixture: ComponentFixture<PbaDetailsComponent>;
+  const emptyServiceStub = () => ({  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [PbaDetailsComponent]
+      declarations: [PbaDetailsComponent],
+      providers: [
+        { provide: PaymentViewService, useFactory: emptyServiceStub }
+      ]
     });
     fixture = TestBed.createComponent(PbaDetailsComponent);
     component = fixture.componentInstance;
