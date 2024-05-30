dd7022dd04f962a221158254e2d8ad88aacdf975
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-05-30 13:49:31 +0100

    PAY-7130 mock HttpClient

--- projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.spec.ts
+++ projects/payment-lib/src/lib/services/shared/httpclient/webcomponent.http.client.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { WebComponentHttpClient } from './webcomponent.http.client';
+import { HttpClient} from "@angular/common/http";
 
 describe('PaymentService', () => {
+
+  const httpClientMock = jasmine.createSpyObj<HttpClient>('HttpClient', ['get']);
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [WebComponentHttpClient]
+      providers: [
+        WebComponentHttpClient,
+        {
+          provide: HttpClient, useValue: httpClientMock
+        }
+      ]
     });
   });
 
