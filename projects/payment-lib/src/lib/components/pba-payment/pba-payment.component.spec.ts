a773f80625bca73e477646674635b6e7240b70aa
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-05-30 11:46:19 +0100

    PAY-7130 stub PaymentViewService

--- projects/payment-lib/src/lib/components/pba-payment/pba-payment.component.spec.ts
+++ projects/payment-lib/src/lib/components/pba-payment/pba-payment.component.spec.ts
@@ -5,6 +5,7 @@ import {PbaPaymentComponent} from './pba-payment.component';
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {RouterTestingModule} from '@angular/router/testing';
 import {NO_ERRORS_SCHEMA, Pipe, PipeTransform} from '@angular/core'
+import {PaymentViewService} from "../../services/payment-view/payment-view.service";
 
 @Pipe({name: 'rpxTranslate'})
 class RpxTranslateMockPipe implements PipeTransform {
@@ -17,12 +18,14 @@ describe('PBA payment component', () => {
   let component: PbaPaymentComponent,
     fixture: ComponentFixture<PbaPaymentComponent>;
   const paymentLibComponentStub = () => ({ viewName: {} });
+  const emptyServiceStub = () => ({  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RpxTranslateMockPipe],
       providers: [
-        {provide: 'PAYMENT_LIB', useFactory: paymentLibComponentStub}
+        {provide: 'PAYMENT_LIB', useFactory: paymentLibComponentStub},
+        {provide: PaymentViewService, useFactory: emptyServiceStub}
       ],
       imports: [
         CommonModule,
