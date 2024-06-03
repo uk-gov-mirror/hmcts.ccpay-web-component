47ca0255ec1dc81525c6bc6cf64a95202d661638
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-06-03 13:33:57 +0100

    PAY-7130 use ChromeHeadless not ChromeDebug. Remove reporters.

--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,28 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const karmaJasmine = require('karma-jasmine');
-const karmaJasmineHtmlReporter = require('karma-jasmine-html-reporter');
-const karmaPhantomJsLauncher = require('karma-phantomjs-launcher');
-const karmaIntlShim = require('karma-intl-shim');
-const karmaCoverageInstanbulReporter = require('karma-coverage-istanbul-reporter');
-const karmaAngularPluginsKarma = require('@angular-devkit/build-angular/plugins/karma');
-const karmaChromeLauncher = require('karma-chrome-launcher');
-const karmaCustomLogger = require('karma-spec-reporter');
-
-
-module.exports = config => {
+module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular', 'intl-shim'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      karmaJasmine,
-      karmaJasmineHtmlReporter,
-      karmaPhantomJsLauncher,
-      karmaIntlShim,
-      // require('./en-us.js'),
-      karmaCoverageInstanbulReporter,
-      karmaAngularPluginsKarma,
-      karmaChromeLauncher,
-      karmaCustomLogger
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    // leave Jasmine Spec Runner output visible in browser
-    client: { clearContext: false },
-    customLaunchers: {
-      ChromeDebug: {
-        base: 'Chrome',
-        flags: [ '--remote-debugging-port=9333', '--headless'],
-        debug: true
-      }
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageReporter: {
-      dir: 'coverage/',
-      type: 'lcov',
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    reporters: ['spec', 'kjhtml', 'coverage-istanbul'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_DEBUG,
-    autoWatch: false,
-    browsers: [ 'ChromeDebug' ],
-    singleRun: true,
-    watch: false
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true
   });
 };
